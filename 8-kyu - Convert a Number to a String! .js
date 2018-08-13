3e093160d709af8a20469014220ad5c7805447c1
Malcolm MacLure <39014058+MacLure@users.noreply.github.com>
2018-08-13 13:18:53 -0400

    Add files via upload

---
+++ 8-kyu - Convert a Number to a String! .js
@@ -0,0 +1,13 @@
+function numberToString(num) {
+  return num.toString();
+}
+
+// function numberToString(num) {
+//   return String(num);
+// }
+
+// function numberToString(num) {
+//     return `${num}`;
+//   }
+
+//   numberToString = (num) => num + ''
