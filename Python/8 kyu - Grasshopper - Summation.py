9b85c040d13c77d2fc9b2117b32790c0fe7c688a
Malcolm <malcolm.maclure@gmail.com>
2019-03-05 22:32:07 -0500

    added kata

---
+++ Python/8 kyu - Grasshopper - Summation.py
@@ -0,0 +1,5 @@
+def summation(num):
+    output = 0
+    for i in range(0, num + 1):
+        output += i
+    return output
---
+++ Python/8 kyu - Sum Mixed Array.py
@@ -0,0 +1,5 @@
+def sum_mix(arr):
+    output = 0
+    for num in arr:
+        output += int(num)
+    return output
